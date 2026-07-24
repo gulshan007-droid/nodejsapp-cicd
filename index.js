1e67806439493866d3ed7892aba26f1b4376512a
Gulshan Verma <gulshanverma@Gulshans-MacBook-Air.local>
2026-07-24 20:11:21 +0530

    added cicd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.url === "/" && req.method === "GET") {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World");
+    res.end("Hello World ghhh");
   } else {
     res.writeHead(404);
     res.end("Not Found");
