d22356f24b176e3b7fb3f78be2330af52552413c
Gulshan Verma <gulshanverma@Gulshans-MacBook-Air.local>
2026-07-24 18:40:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+  } else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
+});
+
+server.listen(4200, () => {
+  console.log("Server running on port 4200");
+});
